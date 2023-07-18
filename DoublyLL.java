85d2281fe56616d37002b4b63c46051a9b002ba3
KAMAKSHI <kkamakshiojha@gmail.com>
2023-07-18 21:02:17 +0530

    Doubly Linked List

---
+++ DoublyLL.java
@@ -0,0 +1,48 @@
+public class DoublyLL {
+    private Node head;
+    private Node tail;
+    private int length;
+
+    public DoublyLL(int value){
+        Node newNode = new Node(value);
+        head = newNode;
+        tail = newNode;
+        length = 1;
+
+
+    }
+    class Node {
+        int value;
+        Node next;
+        Node prev;
+        Node(int value){
+        this.value = value;
+    }
+    }
+
+    public void printLL(){
+        Node temp =head;
+        while(temp !=null){
+            System.out.println(temp.value);
+            temp = temp.next;
+        }
+    }
+
+    public void append(int value){
+        Node n = new Node(value);
+        if(length == 0){
+            head = n;
+            tail = n;
+        }
+        else{
+            tail.next = n;
+            n.prev = tail;
+            tail = n;
+        }
+        length ++;
+
+        
+
+    }
+    
+}
---
+++ Main_DoublyLL.java
@@ -0,0 +1,8 @@
+public class Main_DoublyLL {
+    public static void main(String[] args) {
+        DoublyLL l = new DoublyLL(7);
+        l.append(4);
+        l.append(5);
+        l.printLL();
+    }
+}
