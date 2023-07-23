db069fc4768fdca12c213df8c3036750eb8bd69f
KAMAKSHI <kkamakshiojha@gmail.com>
2023-07-23 10:56:59 +0530

    linear search

---
+++ Linear_search.java
@@ -0,0 +1,28 @@
+public class Linear_search {
+    public static void main(String[] args){
+        int[] array = {10,20,30,-11,15,26};
+        int target = 30;
+        int ans = linear_search(array, target);
+        System.out.println(ans);
+
+    }
+
+    static int linear_search(int[] arr , int target){
+        if(arr.length == 0){
+            return -1;
+        }
+
+        for(int index =0 ; index < arr.length; index++){
+            int element = arr[index];
+            if(element == target){
+                return index;
+            }
+
+        }
+        return -1;
+
+
+    }
+}
+
+
