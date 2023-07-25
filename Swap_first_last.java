0bbc7e0ad46b78a31a67892ab85cb9ebd23f2bbc
KAMAKSHI <kkamakshiojha@gmail.com>
2023-07-25 22:08:17 +0530

    Swap first and last element of DoublyLL

---
+++ Swap_first_last.java
@@ -0,0 +1,60 @@
+public class Swap_first_last {
+    private Node head;
+    private Node tail;
+    private int length;
+
+    public Swap_first_last(int value){
+        Node newNode = new Node(value);
+        head = newNode;
+        tail = newNode;
+        length = 1;
+
+
+    }
+    class Node {
+        int value;
+        Node next;
+        Node prev;
+        Node(int value){
+        this.value = value;
+    }
+    }
+
+    public void printLL(){
+        Node temp =head;
+        while(temp !=null){
+            System.out.println(temp.value);
+            temp = temp.next;
+        }
+
+    }
+    // add element to last/end
+    public void append(int value){
+        Node n = new Node(value);
+        if(length == 0){
+            head = n;
+            tail = n;
+        }
+        else{
+            tail.next = n;
+            n.prev = tail;
+            tail = n;
+        }
+        length ++;
+
+    }
+    public void swapFirstLast(){
+        if(length>2){
+            Node first = head;
+            Node last = tail;
+            head = last;
+            tail = first;
+
+
+        }else{
+            System.out.println("nothing to swap");
+        }
+        printLL();
+    }
+
+}
---
+++ Swap_first_last_main.java
@@ -0,0 +1,12 @@
+public class Swap_first_last_main {
+    public static void main(String[] args) {
+        Swap_first_last l = new Swap_first_last(7);
+        l.append(4);
+        l.append(5);
+        l.append(4);
+        l.append(5);
+        l.swapFirstLast();
+
+
+}
+}
