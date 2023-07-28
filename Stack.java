05d61ecf0f6d5421d1865a5745d86be73011e373
KAMAKSHI <kkamakshiojha@gmail.com>
2023-07-28 21:25:04 +0530

    push elements in stack

---
+++ Stack.java
@@ -0,0 +1,36 @@
+public class Stack {
+    private Node top;
+    private int height;
+
+    class Node{
+        int value;
+        Node next;
+        Node(int value){
+            this.value = value;
+        }
+    }
+    public Stack(int value){
+        Node newNode = new Node(value);
+        top = newNode;
+        height =1;
+
+    }
+    public void printStack(){
+        Node temp = top;
+        while(temp !=null){
+            System.out.println(temp.value);
+            temp = temp.next;
+        }
+    }
+    public void push(int value){
+        Node newNode = new Node(value);
+        if(height == 0){
+            top = newNode;
+        }
+        else{
+            newNode.next = top;
+            top = newNode;
+        }
+        height++;
+    }
+}
---
+++ Stack_main.java
@@ -0,0 +1,10 @@
+public class Stack_main {
+    public static void main(String[] args) {
+        Stack s = new Stack(2);
+        s.push(10);
+        s.push(3);
+        s.push(9);
+        s.printStack();
+
+    }
+}
