000e50a50c91ddaff3efb72a8a9ed5565906e882
KAMAKSHI <kkamakshiojha@gmail.com>
2023-08-01 22:19:19 +0530

    add element in Queue-enqueue

---
+++ Queue.java
@@ -0,0 +1,43 @@
+public class Queue {
+    private Node first; 
+    private Node last;
+    private int length;
+
+    class Node{
+        int value;
+        Node next;
+        Node(int value){
+            this.value = value;
+        }
+    }
+
+    public Queue(int value){
+        Node newNode = new Node(value);
+        first = newNode;
+        last = newNode;
+        length = 1;
+    }
+
+    public void printQueue(){
+        Node temp = first;
+        while(temp != null){
+            System.out.println(temp.value);
+            temp = temp.next;
+        }
+    }
+
+    // add element to queue
+    public void enqueue(int value){
+        Node temp = new Node(value);
+        if(length == 0){
+            first = temp;
+            last = temp;
+        }
+        else{
+            last.next = temp;
+            temp.next = null;
+            last = temp;
+        }
+        length ++;
+    }
+}
---
+++ Queue_main.java
@@ -0,0 +1,8 @@
+public class Queue_main {
+    public static void main(String[] args) {
+        Queue q = new Queue(7);
+        q.enqueue(4);
+        q.enqueue(5);
+        q.printQueue();
+    }
+}
