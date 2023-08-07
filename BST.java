3203b17046b98be76503976b44f31f20bfc78c73
KAMAKSHI <kkamakshiojha@gmail.com>
2023-08-07 20:53:09 +0530

    insert values in a tree

--- BST.java
+++ BST.java
@@ -13,5 +13,33 @@ public class BST {
             this.value = value;
         }
     }
+
+    public boolean insert(int value){
+        Node newNode = new Node(value);
+        if(root == null){
+            root = newNode;
+            return true;
+        }
+        Node temp = root;
+        while(true){
+            if(newNode.value == temp.value) return false;
+            else if(newNode.value < temp.value){
+                if(temp.left == null){
+                    temp.left = newNode;
+                    return true;
+                }
+                temp.left = newNode;
+            }
+            else{
+                if(temp.right == null){
+                    temp.right = newNode;
+                    return true;
+                }
+                temp.right = newNode;
+            }
+        }
+    }
+
+    
     
 }
--- BST_main.java
+++ BST_main.java
@@ -1,6 +1,8 @@
 public class BST_main {
     public static void main(String[] args) {
         BST b = new BST();
-        
+        b.insert(5);
+        b.insert(6);
+        b.insert(10);
     }
 }
