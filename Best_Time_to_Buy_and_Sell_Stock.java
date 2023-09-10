c50d0d81d3b9aba93ab3cd5a6202501aa2c6c5fe
KAMAKSHI <kkamakshiojha@gmail.com>
2023-09-10 11:32:07 +0530

    Best Time to Buy and Sell Stock.

--- Best_Time_to_Buy_and_Sell_Stock.java
+++ Best_Time_to_Buy_and_Sell_Stock.java
@@ -29,3 +29,49 @@ public class Best_Time_to_Buy_and_Sell_Stock {
     return profit;
 }
 }
+
+
+
+//this code is correct but will not work for [2,4,1] type of cases.
+// import java.util.*;
+// class Solution {
+//     public int maxProfit(int[] prices) {
+//         int n = prices.length;
+//         int min = prices[0];
+//         int  x= 0;
+//         int profit = 0;
+//         for(int i = 0;i<n;i++){
+//             if(min>prices[i]){
+//                 min = prices[i];
+//                 x = i; 
+//             }
+//         }
+//         int max = min;
+//         for(int j = x;j<n;j++){
+//             if(max<prices[j]){
+//                 max = prices[j];
+//             }
+//         }
+//         int secmin = Integer.MAX_VALUE;  
+//         if(max == min){
+//             profit = 0;
+//             for(int i = 0;i<n;i++){
+//                 if(prices[i] != min && prices[i] < secmin)
+//                 secmin = prices[i];
+//                 x = i;
+                
+//             }
+//             max = secmin; 
+//             for(int j = x;j<n;j++){
+//             if(max<prices[j]){
+//                 max = prices[j];
+//                 }
+//             }
+//             profit = max - secmin;
+//             return profit;
+//         }
+//         profit = max - min;
+//         return profit;
+        
+//     }
+// }
