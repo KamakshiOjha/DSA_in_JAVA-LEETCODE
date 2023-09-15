f2a0252f1ef2d917325a4ebf88f774992bd060e5
KAMAKSHI <kkamakshiojha@gmail.com>
2023-09-15 21:08:44 +0530

    Valid Anagram

--- valid_string.java
+++ valid_string.java
@@ -1,6 +1,6 @@
 //leetcode - Valid Anagram - Given two strings s and t, return true if t is an anagram of s, and false otherwise.
 
-An Anagram is a word or phrase formed by rearranging the letters of a different word or phrase, typically using all the original letters exactly once.
+//An Anagram is a word or phrase formed by rearranging the letters of a different word or phrase, typically using all the original letters exactly once.
 import java.util.Arrays;
 
 public class  valid_string{
