59ff173c7e2550942b7e8bf3113b65570dc49cff
Kamakshi  Ojha <114620432+KamakshiOjha@users.noreply.github.com>
2024-01-23 23:47:36 +0530

    Update Majority_element.java

--- Majority_element.java
+++ Majority_element.java
@@ -8,32 +8,45 @@ public class Majority_element {
     }
 
     public static int findMajority(int[] arr, int n) {
-        int candidate = arr[0]; 
-        int count = 1; 
+        // int candidate = arr[0]; 
+        // int count = 1; 
 
 
-        for (int i = 1; i < n; i++) {
-            if (arr[i] == candidate) {
-                count++;
-            } else {
-                count--;
-                if (count == 0) {
-                    candidate = arr[i];
-                    count = 1;
-                }
+        // for (int i = 1; i < n; i++) {
+        //     if (arr[i] == candidate) {
+        //         count++;
+        //     } else {
+        //         count--;
+        //         if (count == 0) {
+        //             candidate = arr[i];
+        //             count = 1;
+        //         }
+        //     }
+        // }
+        // count = 0;
+        // for (int i = 0; i < n; i++) {
+        //     if (arr[i] == candidate) {
+        //         count++;
+        //     }
+        // }
+
+        // if (count > n / 2) {
+        //     return candidate;
+        // }
+
+        // return -1; 
+        // optimised 
+
+        int n = nums.length/2;
+        HashMap<Integer,Integer> count = new HashMap<>();
+        for(int i = 0;i<nums.length;i++){
+            count.put(nums[i],count.getOrDefault(nums[i], 0)+1);
+        }
+        for(Map.Entry<Integer,Integer> entry : count.entrySet()){
+            if(entry.getValue()>n){
+                return entry.getKey();
             }
         }
-        count = 0;
-        for (int i = 0; i < n; i++) {
-            if (arr[i] == candidate) {
-                count++;
-            }
-        }
-
-        if (count > n / 2) {
-            return candidate;
-        }
-
-        return -1; 
+        return -1;
     }
 }
