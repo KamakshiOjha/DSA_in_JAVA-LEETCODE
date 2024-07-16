7179f4e3f7f82593ee6f7183d961981be9b167a6
Kamakshi  Ojha <114620432+KamakshiOjha@users.noreply.github.com>
2024-07-16 20:51:15 +0530

    Create L_189.java

---
+++ L_189.java
@@ -0,0 +1,19 @@
+class Solution {
+    public void rotate(int[] nums, int k) {
+        int n = nums.length;
+        k = k % n; 
+        reverse(nums, 0, n - 1);
+        reverse(nums, 0, k - 1);
+        reverse(nums, k, n - 1);
+    }
+
+    public void reverse(int[] arr, int start, int end) {
+        while (start < end) {
+            int temp = arr[start];
+            arr[start] = arr[end];
+            arr[end] = temp;
+            start++;
+            end--;
+        }
+    }
+}
