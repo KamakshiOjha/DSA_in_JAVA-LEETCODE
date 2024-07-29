bfd743e56992b40aba5377985b2880e4709c0cde
Kamakshi  Ojha <114620432+KamakshiOjha@users.noreply.github.com>
2024-07-29 15:05:04 +0530

    Create L_48.java

---
+++ L_48.java
@@ -0,0 +1,21 @@
+class Solution {
+    public void rotate(int[][] matrix) {
+        int m = matrix.length;
+        int n = matrix[0].length;
+        for(int i = 0;i<m-1;i++){
+            for(int j = i+1;j<m;j++){
+                int temp = matrix[i][j];
+                matrix[i][j] = matrix[j][i];
+                matrix[j][i] = temp;
+            }
+        }
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix.length / 2; j++) {
+                int temp = 0;
+                temp = matrix[i][j];
+                matrix[i][j] = matrix[i][matrix.length - 1 - j];
+                matrix[i][matrix.length - 1 - j] = temp;
+            }
+        }
+    }
+}
