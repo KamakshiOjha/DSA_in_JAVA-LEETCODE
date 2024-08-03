853cd43f38eaef6e0c3f7279946fde91514f07b3
Kamakshi  Ojha <114620432+KamakshiOjha@users.noreply.github.com>
2024-08-03 19:57:59 +0530

    Update L_1460.java

--- L_1460.java
+++ L_1460.java
@@ -1,5 +1,6 @@
 class Solution {
     public boolean canBeEqual(int[] target, int[] arr) {
+        // optimal 
         Arrays.sort(target);
         Arrays.sort(arr);
         for(int i = 0;i<target.length;i++){
@@ -7,5 +8,17 @@ class Solution {
                 return false;
             }
         }return true;
+        // better 
+        // HashMap<Integer,Integer> tar = new HashMap<>();
+        // HashMap<Integer,Integer> a = new HashMap<>();
+        // for(int i = 0;i<target.length;i++){
+        //     tar.put(target[i],tar.getOrDefault(target[i],0)+1);
+        // }
+        // for(int i = 0;i<arr.length;i++){
+        //     a.put(arr[i],a.getOrDefault(arr[i],0)+1);
+        // }
+        // if(tar.equals(a)){
+        //     return true;
+        // }return false;
     }
 }
