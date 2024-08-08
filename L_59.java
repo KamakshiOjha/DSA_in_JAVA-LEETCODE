314140214a0bc84b569a106d15a3aa2f2b47385a
Kamakshi  Ojha <114620432+KamakshiOjha@users.noreply.github.com>
2024-08-08 23:35:36 +0530

    Update L_59.java

--- L_59.java
+++ L_59.java
@@ -56,31 +56,22 @@ class Solution {
         int index = 0;
         int count = 1;
         while(top<=bottom && left<=right){
-            // if(index == 0){
                 for(int i = left;i<=right;i++){
                     result[top][i] = count++;
             }
             top++;
-            // }
-            // if(index == 1){
                 for(int i = top;i<=bottom;i++){
                     result[i][right] = count++;
             }
             right--;
-            // }
-            // if(index == 2){
                 for(int i = right;i>=left;i--){
                     result[bottom][i] = count++;
             }
             bottom--;
-            // }
-            // if(index == 3){
                 for(int i = bottom;i>=top;i--){
                     result[i][left] = count++;
             }
             left++;
-            // }
-            index = (index+1)%4;
         }
         return result;
     }
