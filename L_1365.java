3c64948e6baac35310e3a904114c9923bceabe5b
Kamakshi  Ojha <114620432+KamakshiOjha@users.noreply.github.com>
2024-08-17 20:35:05 +0530

    Create L_1365.java

---
+++ L_1365.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int[] smallerNumbersThanCurrent(int[] nums) {
+        int n = nums.length;
+        int[]arr= new int[n];
+        for(int i = 0;i<n;i++){
+            int count = 0;
+            for(int j = 0;j<n;j++){
+                if(i!=j && nums[j] < nums[i]){
+                    count++;
+                }
+            }arr[i]=count;
+            
+        }
+        return arr;
+    }
+}
