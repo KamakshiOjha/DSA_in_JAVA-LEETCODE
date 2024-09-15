807137c5a8e905d2783bb613ea4bd10914a4da03
Kamakshi  Ojha <114620432+KamakshiOjha@users.noreply.github.com>
2024-09-15 21:18:17 +0530

    Create L_90.java

---
+++ L_90.java
@@ -0,0 +1,19 @@
+class Solution {
+    public List<List<Integer>> subsetsWithDup(int[] nums) {
+        Arrays.sort(nums);
+        List<List<Integer>> res = new ArrayList<>();
+        find(0,res,nums,new ArrayList<>());
+        return res;
+    }
+    public static void find(int ind,List<List<Integer>> res,int[]nums,List<Integer> ds ){
+        res.add(new ArrayList<>(ds));
+        for(int i = ind;i<nums.length;i++){
+            if(ind!=i &&nums[i]==nums[i-1]){
+                continue;
+            }
+            ds.add(nums[i]);
+            find(i+1,res,nums,ds);
+            ds.remove(ds.size()-1);
+        }
+    }
+}
